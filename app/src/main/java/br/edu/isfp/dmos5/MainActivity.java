d2d28fd65dd6eb706f54cb18820acddaacc40a75
Matheus-OD074 <70017961+Matheus-OD074@users.noreply.github.com>
2023-03-05 17:59:53 -0300

    Versao1 Funcional
    Nesta versao o conversor esta funcional

--- app/src/main/java/br/edu/isfp/dmos5/MainActivity.java
+++ app/src/main/java/br/edu/isfp/dmos5/MainActivity.java
@@ -3,12 +3,60 @@ package br.edu.isfp.dmos5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText tempEditText;
+    private TextView resultTextView;
+    private Button buttonConverterCelsius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempEditText = findViewById(R.id.editText_temperature);
+        resultTextView = findViewById(R.id.textView_Result);
+        buttonConverterCelsius = findViewById(R.id.buttonConverterCelsius);
+        buttonConverterCelsius.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonConverterCelsius){
+            getFahrenheitConversion();
+        }
+    }
+    private void getFahrenheitConversion(){
+        double result;
+        FahrenheitStrategy fs = new FahrenheitStrategy();
+        result = fs.getConversion(getTemp());
+        showResult(result);
+    }
+
+    private double getTemp(){
+        double temp;
+        String stringTemp;
+
+        stringTemp = tempEditText.getText().toString();
+        try{
+            temp = Double.parseDouble(stringTemp);
+        }catch(NumberFormatException e) {
+            Toast.makeText(this, "Valor digitado é invalido.", Toast.LENGTH_SHORT).show();
+            temp = 0;
+        }
+
+        return temp;
+    }
+
+    private void showResult(double result){
+        resultTextView.setText(String.format("%.2f °C", result));
+    }
+
 }
